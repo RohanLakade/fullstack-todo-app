9a3c7fa4a0f1cc723ed3ccfc4aeb1db4a03776da
RohanLakade <lakade.rohan419@gmail.com>
2025-03-27 16:55:06 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://rohanlakade.github.io/fullstack-todo-app",
+      "https://fullstack-todo-app-zeta.vercel.app/",
     ],
     methods: "GET,POST,PUT,PATCH,DELETE",
     credentials: true,
